71504474a14e48a4425ff54dac03e36cc3912121
Vincent Lobo <vincelobo4299@gmail.com>
2018-07-30 10:55:14 -0500

    Update TableGui.java

--- TableGui.java
+++ TableGui.java
@@ -7,22 +7,18 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.table.TableColumnModel;
-public class TableGui extends JFrame implements ActionListener
-{
-	private static final long serialVersionUID = 1L;
-	public JTable table;
+public class TableGui extends JFrame implements ActionListener {
+    private static final long serialVersionUID = 1L;
+    public JTable table;
     private boolean in;
     public Square [][] elbow;
     
     @Override
     public void actionPerformed(ActionEvent ae){}
-    public TableGui()
-    {
+    public TableGui() {
         elbow = new Square[9][9];
-        for (int x=0;x<9;x++)
-        {
-            for(int p=0;p<9;p++)
-            {
+        for (int x=0;x<9;x++) {
+            for(int p=0;p<9;p++) {
                 elbow[x][p]= new Square();
             }
         }
@@ -31,41 +27,30 @@ public class TableGui extends JFrame implements ActionListener
         JTable table = new JTable(9,9);
         table.setRowHeight(40);
         TableColumnModel columnModel = table.getColumnModel();
-        for(int x=0;x<9;x++)
-        {
+        for(int x=0;x<9;x++) {
             columnModel.getColumn(x).setPreferredWidth(15);
         }
         this.setTitle("Enter Known Values");
-        
         JButton b1 = new JButton("Solve!");
         p.add(b1);
-        
         p.add(new JScrollPane(table));
         this.add(p);
-        
         b1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 in = true;
             }
         }); 
-        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
         this.pack();
         this.setVisible(true);
-        
-        //just waits for button to be pressed
         while(in == false){
         }
         setVisible(false);
         
-        for(int a=0;a<9;a++)
-        {
-            for(int b=0;b<9;b++)
-            {
+        for(int a=0;a<9;a++) {
+            for(int b=0;b<9;b++) {
                 String temp = (String) table.getModel().getValueAt(a,b);
-                if(temp!=null&&temp!="")
-                {
+                if(temp!=null&&temp!="") {
                     int schlep = Integer.parseInt(temp);
                     elbow[a][b].finnum = schlep;
                     elbow[a][b].fin=true;
@@ -92,29 +77,23 @@ public class TableGui extends JFrame implements ActionListener
         }
     }
     
-    public Square[][] give()
-    {
+    public Square[][] give() {
         return elbow;
     }
     
-    public TableGui(Square [][] end){
-
+    public TableGui(Square [][] end) {
         JPanel p = new JPanel();
         JTable table = new JTable(9,9);
         table.setRowHeight(40);
         TableColumnModel columnModel = table.getColumnModel();
-        for(int x=0;x<9;x++)
-        {
+        for(int x=0;x<9;x++) {
             columnModel.getColumn(x).setPreferredWidth(15);
         }
         p.add(new JScrollPane(table));
         this.add(p);
-        
-        
-        for(int x=0; x<9;x++)
-        {
-            for(int y=0; y<9;y++)
-            {
+        this.setTitle("Solved Sudoku!");
+        for(int x=0; x<9;x++) {
+            for(int y=0; y<9;y++) {
                 table.setValueAt(end[x][y].finnum,x,y);
             }
         }    
