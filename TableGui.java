959ffc3dbe3f6533a77f9e0847470ace5f66a3c7
Vincent Lobo <vincelobo4299@gmail.com>
2018-07-30 11:57:22 -0500

    Update TableGui.java

--- TableGui.java
+++ TableGui.java
@@ -50,9 +50,7 @@ public class TableGui extends JFrame implements ActionListener {
         	} catch(InterruptedException e) {
         	}
         }
-        setVisible(false);
-        System.out.println("Made it!");
-        
+        setVisible(false);        
         for(int a=0;a<9;a++) {
             for(int b=0;b<9;b++) {
                 String temp = (String) table.getModel().getValueAt(a,b);
@@ -81,7 +79,6 @@ public class TableGui extends JFrame implements ActionListener {
                 }
             }
         }
-        System.out.print("processed");
     }
     
     public Square[][] give() {
