1295b9e59a67bf4ede56425b8dce42590fc6691d
Vincent Lobo <vincelobo4299@gmail.com>
2019-02-04 01:12:40 -0500

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,9 +1,7 @@
 public class Solver
 {
     private Square[][] enter;
-    
-    //TESTING
-    //private int[][] temptest;
+    private int[][] temptest;
     
     public static void main(String[] args) {
     	new Solver();
@@ -16,12 +14,6 @@ public class Solver
                 enter[x][p]= new Square();
             }
         }
-        
-        //USED FOR TESTING
-        
-        //int [][] temptest = {{0,1,5,4,6,9,7,3,8},{4,8,7,1,3,5,6,2,9},{6,9,3,8,7,2,4,1,5},{8,6,2,3,4,1,5,9,7},{5,4,9,2,8,7,3,6,1},{7,3,1,9,5,6,2,8,4},{9,2,4,5,1,3,8,7,6},{1,7,8,6,2,4,9,5,3},{3,5,6,7,9,8,1,4,2}};
-        int [][] temptest = {{2,0,0,3,0,0,0,0,0},{8,0,4,0,6,2,0,0,3},{0,1,3,8,0,0,2,0,0},{0,0,0,0,2,0,3,9,0},{5,0,7,0,0,0,6,2,1},{0,3,2,0,0,6,0,0,0,},{0,2,0,0,0,9,1,4,0},{6,0,1,2,5,0,8,0,9},{0,0,0,0,0,1,0,0,2}};
-        
         for(int t=0;t<9;t++) {
             for(int h=0;h<9;h++) {
                 if(temptest[t][h]!=0) {
@@ -48,9 +40,7 @@ public class Solver
                 }
             }
         }
-        //END TESTING
-        
-        //enter = inputGui();
+        enter = inputGui();
         solutionGui(solvin());
     }
     
